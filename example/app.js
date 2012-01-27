c7e6da3121cab18d24be3d86570b6183e921bec7
Jeff English <jeff@utmostlogic.com>
2012-01-27 17:39:59 -0600

    Merge branch 'master' of github.com:appcelerator/titanium_mobile_modules into MOD-368

--- example/app.js
+++ example/app.js
@@ -1,54 +1,54 @@
 var win = Ti.UI.createWindow({
-    backgroundColor: 'white',
-    exitOnClose: true,
-    navBarHidden: true
+    backgroundColor:'white',
+    exitOnClose:true,
+    navBarHidden:true
 });
 win.add(Ti.UI.createLabel({
-    text: 'Starting up TTS...'
+    text:'Starting up TTS...'
 }));
 win.open();
 
 var TTS = require('ti.tts');
 TTS.start({
-    language: TTS.LOCALE_US,
-    ready: function () {
+    language:TTS.LOCALE_US,
+    ready:function () {
 
         TTS.speak({
-            text: 'This speech interrupts all other speeches.',
-            queue: TTS.QUEUE_FLUSH
+            text:'This speech interrupts all other speeches.',
+            queue:TTS.QUEUE_FLUSH
         });
         TTS.speak({
-            text: 'But this speech is queued.',
-            queue: TTS.QUEUE_ADD
+            text:'But this speech is queued.',
+            queue:TTS.QUEUE_ADD
         });
         TTS.speak({
-            text: 'Queuing is the default behavior.'
+            text:'Queuing is the default behavior.'
         });
         TTS.speak({
-            text: 'We can speak to different streams as well.'
+            text:'We can speak to different streams as well.'
         });
         TTS.speak({
-            text: 'Such as the alarm stream.',
-            stream: TTS.STREAM_ALARM
+            text:'Such as the alarm stream.',
+            stream:TTS.STREAM_ALARM
         });
         TTS.speak({
-            text: 'Or the music stream.',
-            stream: TTS.STREAM_MUSIC
+            text:'Or the music stream.',
+            stream:TTS.STREAM_MUSIC
         });
         TTS.speak({
-            text: 'Speeches can be differentiated with string identifiers.',
-            id: 'wow'
+            text:'Speeches can be differentiated with string identifiers.',
+            id:'wow'
         });
         TTS.pause({
-            milliseconds: 1000
+            milliseconds:1000
         });
         TTS.speak({
-            text: 'When you are done, make sure you call T T S dot shutdown.',
-            id: 'done'
+            text:'When you are done, make sure you call T T S dot shutdown.',
+            id:'done'
         });
         Ti.API.info('Note that calls to speak and pause are asynchronous.');
     },
-    doneSpeaking: function (args) {
+    doneSpeaking:function (args) {
         if (args.id == 'wow') {
             Ti.API.info('The speech with id "wow" just finished!');
         }
@@ -56,10 +56,10 @@ TTS.start({
             TTS.shutdown();
         }
     },
-    failed: function () {
+    failed:function () {
         Ti.UI.createAlertDialog({
-            title: 'TTS Not Installed',
-            message: 'Relaunch this app after installing TTS, please!'
+            title:'TTS Not Installed',
+            message:'Relaunch this app after installing TTS, please!'
         }).show();
     }
 });
